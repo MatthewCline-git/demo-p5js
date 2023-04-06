633c00f9ae6f1533d795b208217cfaebfc746c1c
cnieh49 <40966582+cnieh49@users.noreply.github.com>
2023-04-06 14:14:02 -0400

    assignment 5
    made changes to the colors to make the system more colorblind friendly

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#6c218e');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#8fba06');
   }
   updateSnakeCoordinates();
   updateHandContainer();
